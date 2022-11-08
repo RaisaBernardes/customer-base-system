a599c6fb7bdd1cfdaf58a80cd30e66e7f2fb295a
Raisa Bernardes <raisa.rmro@gmail.com>
2022-11-08 11:59:20 -0300

    design changes

---
+++ script.js
@@ -0,0 +1,12 @@
+'use strict'
+
+//Modal logic
+const openModal = () => {
+    document.getElementById('modal').classList.add('active')
+}
+const closeModal = () => {
+    document.getElementById('modal').classList.remove('active')
+}
+
+document.getElementById('registerCustomer').addEventListener('click', openModal);
+document.getElementById('modalClose').addEventListener('click', closeModal);
