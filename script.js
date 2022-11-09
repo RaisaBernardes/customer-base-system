e3a66fb6d92948dd3b2eb2607f4d7de1ddf6e412
Raisa Bernardes <raisa.rmro@gmail.com>
2022-11-09 15:07:02 -0300

    CRUD's create done

--- script.js
+++ script.js
@@ -8,5 +8,26 @@ const closeModal = () => {
     document.getElementById('modal').classList.remove('active')
 }
 
+const temporaryCustomer = {
+    name: "Raisa",
+    email: "raisa.rmro@gmail.com",
+    phone: "1234598778",
+    city: "Recife"
+}
+
+const getLocalStorage = () => JSON.parse(localStorage.getItem('db_customer')) ?? [];
+const setLocalStorage = (dbCustomer) => localStorage.setItem('db_customer', JSON.stringify(dbCustomer));
+
+//CRUD
+
+//Create
+const createCustomer = (customer) => {
+    const dbCustomer = getLocalStorage();
+    dbCustomer.push(customer);
+    setLocalStorage(dbCustomer)
+}
+
+
+//Events
 document.getElementById('registerCustomer').addEventListener('click', openModal);
 document.getElementById('modalClose').addEventListener('click', closeModal);
